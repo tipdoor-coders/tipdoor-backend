35724fe8c9ef8b5db39ce348a5c1477709b8a9cc
unknown <73161765+adwaithkrishna@users.noreply.github.com>
2025-09-29 23:32:28 +0530

    Update admin

--- tipdoor/shop/admin.py
+++ tipdoor/shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Product, Promotion
+from . import models
 
-admin.site.register(Product)
-admin.site.register(Promotion)
+admin.site.register(models.Product)
+admin.site.register(models.Promotion)
+admin.site.register(models.Order)
