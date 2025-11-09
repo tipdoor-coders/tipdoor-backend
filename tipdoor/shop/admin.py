56bf5e4d8dab3afb44ac26df7d8e88d95397d4c3
unknown <73161765+adwaithkrishna@users.noreply.github.com>
2025-11-09 16:09:03 +0530

    Register all models to admin

--- tipdoor/shop/admin.py
+++ tipdoor/shop/admin.py
@@ -5,3 +5,7 @@ from . import models
 admin.site.register(models.Product)
 admin.site.register(models.Promotion)
 admin.site.register(models.Order)
+admin.site.register(models.Customer)
+admin.site.register(models.Cart)
+admin.site.register(models.CartItem)
+admin.site.register(models.OrderItem)
