eaf30104f652ffffa2e4c0f4be12ce5e37441584
unknown <73161765+adwaithkrishna@users.noreply.github.com>
2025-11-16 19:38:42 +0530

    Fix order model with modified user

--- tipdoor/shop/models.py
+++ tipdoor/shop/models.py
@@ -69,7 +69,7 @@ class Order(models.Model):
     promo_code = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return f"Order {self.id} by {self.user.username}"
+        return f"Order {self.id} by {self.user.name}"
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
